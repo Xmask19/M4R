cb214c5ab833bf431eb724ae1660a0a0e3f9662b
Xmask19 <43860179+Xmask19@users.noreply.github.com>
2025-11-08 18:20:03 +0000

    Stuck probably because something false has been sorried, but it is hopefully fixable without huge trouble.

--- M4R/Basic.lean
+++ M4R/Basic.lean
@@ -1,26 +1,11 @@
-
 import Mathlib.Tactic
--- import Mathlib.Topology.MetricSpace.Pseudo.Defs
+import Mathlib.Topology.Basic
+import Mathlib.Topology.MetricSpace.Basic
 import Mathlib.Analysis.InnerProductSpace.EuclideanDist
+import Mathlib.Data.Real.Basic
+import Mathlib.Algebra.Module.Basic
 
 
-/-- Let f be a continuous function on the unit ball in Euclidean space ℝ^n,
-then f has at least one fixed point -/
-
--- theorem brouwer_fixed_point (n : ℕ)
---         (f : Metric.closedBall (0 : EuclideanSpace ℝ (Fin n)) 1 →
---         Metric.closedBall (0 : EuclideanSpace ℝ (Fin n)) 1)
---         (hf : Continuous f) : ∃ x, f x = x := by sorry
-
-
--- theorem restated_IoD (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n))
---         (hf_cont : Continuous f) (hf_inj : Function.Injective f)
---         : f 0 ∈ interior (f '' (Metric.closedBall (0 : EuclideanSpace ℝ (Fin n)) 1)) := by sorry
-
-
--- theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n))
---         (hf_cont : Continuous f) (hf_inj : Function.Injective f) : IsOpenMap f := by
---     intro U hU
 
 theorem brouwer_fixed_point (n : ℕ) (f : Euclidean.closedBall ((0 : EuclideanSpace ℝ (Fin n))) 1 →
         Euclidean.closedBall 0 1) (hf : Continuous f) : ∃ x, f x = x := by sorry
@@ -31,54 +16,9 @@ lemma zero_mem_closed_ball (n : ℕ) :
     apply Euclidean.ball_subset_closedBall
     exact Euclidean.mem_ball_self zero_lt_one
 
-
--- theorem restated_IoD (n : ℕ)
---     (f : Euclidean.closedBall (0 : EuclideanSpace ℝ (Fin n)) 1 → EuclideanSpace ℝ (Fin n))
---     (hf_cont : Continuous f)
---     (hf_inj : Function.Injective f) :
---     f ⟨0, (zero_mem_closed_ball n)⟩ ∈ interior (Set.range f) := by sorry
-
-
 theorem restated_IoD (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n))
         (hf_cont : Continuous f) (hf_inj : Function.Injective f)
         : f 0 ∈ interior (f ''(Euclidean.closedBall 0 1)) := by sorry
-
--- theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n))
---         (hf_cont : Continuous f) (hf_inj : Function.Injective f) : IsOpenMap f := by
---     intro U hU
---     rw [isOpen_iff_ball_subset]
---     intro y hy
---     rcases hy with ⟨x, hxU, hfx⟩
---     have h := isOpen_iff_ball_subset.mp hU
---     specialize (h) x hxU
---     rcases h with ⟨nbd, hnbd1, hnbd2⟩
---     rw [Metric.mem_uniformity_dist] at hnbd1
---     rcases hnbd1 with ⟨ε, hε1, hε2⟩
---     let g := fun (v : EuclideanSpace ℝ (Fin n)) => f (x + ε • v)
---     have hg_cont : Continuous g :=
---         hf_cont.comp (continuous_const.add (continuous_const.smul continuous_id))
---     have hg_inj : Function.Injective g := by
---         intro v1 v2 h
---         have H2 : ε • v1 = ε • v2 := by
---             have H := hf_inj h
---             rw [add_right_inj] at H
---             exact hf_inj (hf_inj (congrArg f (congrArg f (H))))
---         rw [← sub_eq_zero] at H2 ⊢
---         rw [← smul_sub] at H2
---         rcases smul_eq_zero.mp H2 with (hε | hsub)
---         ·   exfalso
---             exact (ne_of_lt hε1) (id (Eq.symm hε))
---         · exact hsub
-
-
---     have restated := restated_IoD n g hg_cont hg_inj
-
---     have hinc : interior (Euclidean.closedBall 0 1) ⊆ f '' U := by sorry
---     rw [← UniformSpace.mem_nhds_iff]
---     rw [← hfx]
-
---     sorry
-
 
 theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → EuclideanSpace ℝ (Fin n))
         (hf_cont : Continuous f) (hf_inj : Function.Injective f) : IsOpenMap f := by
@@ -96,13 +36,13 @@ theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → Eucli
     have hg_inj : Function.Injective g
      := by
         intro v1 v2 h
-        have H := hf_inj h
-        rw [add_right_inj] at H
-        rw [← sub_eq_zero] at H ⊢
-        rw [← smul_sub] at H
+        have h13 := hf_inj h
+        rw [add_right_inj] at h13
+        rw [← sub_eq_zero] at h13 ⊢
+        rw [← smul_sub] at h13
         have h10 : 0 < ε/2 := half_pos hε1
-        rw [smul_eq_zero] at H
-        rcases H with h11 | h12
+        rw [smul_eq_zero] at h13
+        rcases h13 with h11 | h12
         ·   exfalso
             subst hfx
             simp_all only [gt_iff_lt, zero_smul, add_zero, lt_self_iff_false, g]
@@ -118,8 +58,19 @@ theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → Eucli
         rw [h6, Set.image_comp, Set.image_smul]
         rw [ ← Set.singleton_add]
         have hball_eq : Euclidean.closedBall (0 : EuclideanSpace ℝ (Fin n)) 1 =
-                Metric.closedBall 0 1 := by sorry
-        --     -- ext p
+                Metric.closedBall 0 1 := by
+            ext p
+            simp only [Metric.mem_closedBall, dist_zero_right]
+            unfold Euclidean.closedBall
+            simp only [Set.mem_setOf_eq]
+            rw [Euclidean.dist]
+            rw [dist_eq_norm]
+            simp only [map_zero, sub_zero]
+            -- have h : ‖toEuclidean p‖ = ‖p‖ := sorry
+            -- This is probably not true as, toEuclidean is a continous ℝ linear equivalence
+            -- so it might be that p doesn't necessarily go to p but instead to some c*p?
+            -- Hopefully this can be fixed via multiplying by a constant or something simple
+            sorry
         rw [hball_eq]
         rw [smul_closedBall]
         ·   simp only [Real.norm_eq_abs, singleton_add_closedBall]
@@ -133,20 +84,7 @@ theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → Eucli
             · exact hε2
         · exact zero_le_one' ℝ
 
-        -- simp only [smul_zero, add_zero]
-        -- · sorry
-        -- · sorry
-        -- rw [affinity_unitClosedBall (by linarith)]
-        -- rw [h6, Set.image_comp]
-        -- rw [Set.image_smul,  Set.image_add_left]
-        -- rw [Set.image.smul]
-        -- rw [affinity_unitClosedBall]
-
     have h3 : f '' nbd ⊆ f '' U := Set.image_mono hnbd1
-    -- have h4 : g '' (Euclidean.ball (0 : EuclideanSpace ℝ (Fin n)) 1) ⊆ f '' U := by
-    --     rw [h1, Set.image_comp]
-    --     exact (Set.image_mono h2).trans h3
-
     have h4 : g '' (Euclidean.closedBall (0 : EuclideanSpace ℝ (Fin n)) 1) ⊆ f '' U := by
         rw [h1]
         rw [Set.image_comp]
@@ -167,16 +105,3 @@ theorem invariance_of_domain (n : ℕ) (f : EuclideanSpace ℝ (Fin n) → Eucli
     ·   unfold g at ha2
         rw [smul_zero, add_zero] at ha2
         exact Set.mem_of_eq_of_mem (hf_inj (congrArg f (id (Eq.symm hfx)))) ha2
-
-    -- use g '' (Euclidean.ball 0 1)
-    -- constructor
-    -- · sorry
-    -- constructor
-    -- · sorry
-    -- ·   rw [ Set.mem_image]
-    --     use 0
-    --     constructor
-    --     · exact Euclidean.mem_ball_self (by simp only [zero_lt_one])
-    --     ·   unfold g
-    --         rw [smul_zero, add_zero]
-    --         exact hf_inj (hf_inj (congrArg f (congrArg f hfx)))
